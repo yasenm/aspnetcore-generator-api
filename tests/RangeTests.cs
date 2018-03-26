126b1c83010d185040e4beef70017be1cd08231d
yasenm <ias_sv_bg@yahoo.co.uk>
2018-03-26 14:03:36 +0300

    Add sort test

--- tests/RangeTests.cs
+++ tests/RangeTests.cs
@@ -16,5 +16,16 @@ namespace tests
 
             Assert.Equal(3, generate.Count());
         }
+
+        [Fact]
+        public void SortShouldOrderResult()
+        {
+            var range = new Range { Count = 3, Sort = true };
+            var values = new[] {"a","c", "b"};
+            var counter = 0;
+            var generate = range.Of(() => values[counter++]);
+
+            Assert.Equal(new[] {"a","b", "c"}, generate.Count());
+        }
     }
 }
